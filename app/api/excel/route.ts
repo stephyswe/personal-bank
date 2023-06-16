5e1ba73e93a6d081768a0ae960d7ab851d5dddcf
Stephanie <stephaniiehallberg@gmail.com>
2023-06-16 23:04:56 +0200

    feat(app): update to typescript

--- app/api/excel/route.ts
+++ app/api/excel/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 import { join } from "path";
+import fs from "fs";
 
 import { getExcel } from "../../../utils/api/getExcel";
 
-export async function GET(request) {
+export async function GET() {
   const filePath = join(process.cwd(), "kontoutdrag.xlsx");
   const data = fs.existsSync(filePath) ? getExcel(filePath) : [];
   return NextResponse.json(data);
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ export const metadata = {
 
 export default function RootLayout({
   children,
+}: {
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
